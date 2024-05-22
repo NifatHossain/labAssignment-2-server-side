5205cf2c3648cfe1a11a80003e5c80df9e1d6480
NifatHossain <nifathossain1480@gmail.com>
2024-05-22 15:06:59 +0600

    Added update Api

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.get('/allusers/:email', function (req, res) {
     // })
 });
 
+//delete from  users table
 app.delete('/deleteuser/:id',(req,res)=>{
     const id=req.params.id;
     const sql= `delete from users where id='${id}'`
@@ -80,11 +81,19 @@ app.delete('/deleteuser/:id',(req,res)=>{
         res.send(result)
     })
 })
-// POST API end point defination
-// app.post('/test-post', function (req, res) {
-//     console.log(req.body);
-//     return res.json('success');
-// });
+//delete from current user table
+app.delete('/deletecurrentuser',(req,res)=>{
+    const id=req.params.id;
+    var sql= `delete from currentuser`;
+        connection.query(sql,(error,result)=>{
+            if(error){
+                console.log(error)
+            }
+            // console.log(result)
+            res.send(result)
+        })
+})
+
 
 // DB
 app.post('/addcurrentuser', function (req, res) {
@@ -122,6 +131,27 @@ app.post('/addcurrentuser', function (req, res) {
         }
     );
 })
+
+app.post('/updatedata/:id',(req,res)=>{
+    const paramId= req.params.id;
+    const user = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        password: req.body.password,
+        image: req.body.image
+    };
+    console.log(user)
+    const sql= `update users set fName="${user.fname}", lName="${user.lname}", birthDate="${user.dob}", gender="${user.gender}", password="${user.password}", image="${user.image}" where id=${paramId}`
+    connection.query(sql,(error,result)=>{
+        if(error){
+            console.log(error)
+        }
+        res.send(result)
+    })
+})
+
 app.post('/adduser', function (req, res) {
     console.log("here-----");
 
@@ -131,12 +161,13 @@ app.post('/adduser', function (req, res) {
         email: req.body.email,
         dob: req.body.dob,
         gender: req.body.gender,
-        password: req.body.password
+        password: req.body.password,
+        image: req.body.image
     };
 
     console.log("user", user);
 
-    connection.query(`INSERT INTO users (fName, lName, gender, birthDate, email, password) VALUES ('${user.fname}', '${user.lname}', '${user.gender}', '${user.dob}','${user.email}', '${user.password}');`,
+    connection.query(`INSERT INTO users (fName, lName, gender, birthDate, email, password,image) VALUES ('${user.fname}', '${user.lname}', '${user.gender}', '${user.dob}','${user.email}', '${user.password}', '${user.image}');`,
         function (err, result) {
             if (err) {
                 console.log("err", err);
